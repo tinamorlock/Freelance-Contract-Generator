6a7641e83d80bc5fe4a1eec55884ef35f615eaa8
tinamorlock <tinamorlock@gmail.com>
2024-02-09 19:44:12 -0600

    started on script code for export and html generating

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-const Export2Word = (element, filename = '') => {
+const inputArr = [
+    'freelancerName',
+    'freelancerAddress',
+    'freelancerCityStateZip',
+    'freelancerEmail',
+    'freelancerPhone',
+    'clientName',
+    'clientAddress',
+    'clientCityStateZip',
+    'clientEmail',
+    'clientPhone',
+    'projectDescription',
+    'service1',
+    'service2',
+    'service3',
+    'service4',
+    'service5',
+    'projectCost',
+    'projectHourly',
+    'deliverable1',
+    'deliverable2',
+    'deliverable3',
+    'deliverable4',
+    'deliverable5',
+    'projectStart',
+    'projectEnd',
+    'customSection1',
+    'customText1',
+    'customSection2',
+    'customText2',
+    'customSection3',
+    'customText3',
+    'customSection4',
+    'customText4',
+    'customSection5',
+    'customText5',
+];
+const genContract = document.querySelector('#generateContract');
+
+const export2Word = (element, filename = '') => {
     var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
     var postHtml = "</body></html>";
     var html = preHtml + document.getElementById(element).innerHTML + postHtml;
@@ -30,3 +69,11 @@ const Export2Word = (element, filename = '') => {
 
     document.body.removeChild(downloadLink);
 }
+
+const makeContract = () => {
+    // enter code for displaying contract on page
+}
+
+// event listeners
+
+genContract.addEventListener('click', makeContract);
