780a1cd94f42bcc251302a34e7b40e0d476997ef
tinamorlock <tinamorlock@gmail.com>
2026-09-12 18:46:06 -0500

    Fixed variables.

--- script.js
+++ script.js
@@ -80,77 +80,79 @@ const makeContract = () => {
     // services HTML to account for multiple services entered
     // only adds them if entered
     let servicesHTML = "";
-    if (inputs.service1) {
-        servicesHTML += `<li>${service1}</li>`;
+    if (inputs["service1"]) {
+        servicesHTML += `<li>${inputs["service1"]}</li>`;
     }
-    if (inputs.service2) {
-        servicesHTML += `<li>${service2}</li>`;
+    if (inputs["service2"]) {
+        servicesHTML += `<li>${inputs["service2"]}</li>`;
     }
-    if (inputs.service3) {
-        servicesHTML += `<li>${service3}</li>`;
+    if (inputs["service3"]) {
+        servicesHTML += `<li>${inputs["service3"]}</li>`;
     }
-    if (inputs.service4) {
-        servicesHTML += `<li>${service4}</li>`;
+    if (inputs["service4"]) {
+        servicesHTML += `<li>${inputs["service4"]}</li>`;
     }
-    if (inputs.service5) {
-        servicesHTML += `<li>${service5}</li>`;
+    if (inputs["service5"]) {
+        servicesHTML += `<li>${inputs["service5"]}</li>`;
     }
 
     // deliverables HTML to account for multiple deliverables
     // only adds them if entered
     let deliverHTML = "";
-    if (inputs.deliverable1) {
-        deliverHTML += `<li>${deliverable1}</li>`;
+    if (inputs["deliverable1"]) {
+        deliverHTML += `<li>${inputs["deliverable1"]}</li>`;
     }
-    if (inputs.deliverable2) {
-        deliverHTML += `<li>${deliverable2}</li>`;
+    if (inputs["deliverable2"]) {
+        deliverHTML += `<li>${inputs["deliverable2"]}</li>`;
     }
-    if (inputs.deliverable3) {
-        deliverHTML += `<li>${deliverable3}</li>`;
+    if (inputs["deliverable3"]) {
+        deliverHTML += `<li>${inputs["deliverable3"]}</li>`;
     }
-    if (inputs.deliverable4) {
-        deliverHTML += `<li>${deliverable4}</li>`;
+    if (inputs["deliverable4"]) {
+        deliverHTML += `<li>${inputs["deliverable4"]}</li>`;
     }
-    if (inputs.deliverable5) {
-        deliverHTML += `<li>${deliverable5}</li>`;
+    if (inputs["deliverable5"]) {
+        deliverHTML += `<li>${inputs["deliverable5"]}</li>`;
     }
 
     // building the full contract
     const contractHTML = `
         <h1>Freelance Services Agreement</h1>
-        <p>This agreement is between ${freelancerName}
-        and ${clientName}</p>
+        <p>This agreement is between ${inputs["freelancerName"]}
+        and ${inputs["clientName"]}</p>
         <h2>Contact Information</h2>
         <h3>Freelancer</h3>
-        <p>Name: ${freelancerName}</p>
+        <p>Name: ${inputs["freelancerName"]}</p>
         <p>Address:</p>
-        <p>${freelancerAddress}</p>
-        <p>${freelancerCityStateZip}</p>
-        <p>Phone Number: ${freelancerPhone}</p>
-        <p>Email: <a href="mailto:${freelancerEmail}">${freelancerEmail}</a></p>
+        <p>${inputs["freelancerAddress"]}</p>
+        <p>${inputs["freelancerCityStateZip"]}</p>
+        <p>Phone Number: ${inputs["freelancerPhone"]}</p>
+        <p>Email: <a href="mailto:${inputs['freelancerEmail']}">${inputs["freelancerEmail"]}</a></p>
         <h3>Client</h3>
-        <p>Name: ${clientName}</p>
+        <p>Name: ${inputs["clientName"]}</p>
         <p>Address:</p>
-        <p>${clientAddress}</p>
-        <p>${clientCityStateZip}</p>
-        <p>Phone Number: ${clientPhone}</p>
-        <p>Email: <a href="mailto:${clientEmail}">${clientEmail}</a></p>
+        <p>${inputs["clientAddress"]}</p>
+        <p>${inputs["clientCityStateZip"]}</p>
+        <p>Phone Number: ${inputs["clientPhone"]}</p>
+        <p>Email: <a href="mailto:${inputs['clientEmail']}">${inputs["clientEmail"]}</a></p>
         <h2>Project Information</h2>
         <p><i>
-            ${projectDescription}
+            ${inputs["projectDescription"}
         </i></p>
         <h3>Services</h3>
         <ul>
             ${servicesHTML}
         </ul>
         <h3>Pricing</h3>
-        <p>Estimated project fee: $${projectCost}</p>
-        <p>Additional work billed at $${projectHourly} per hour.</p>
+        <p>Estimated project fee: $${inputs["projectCost"}</p>
+        <p>Additional work billed at $${inputs["projectHourly"]} per hour.</p>
         <h2>Deliverables</h2>
         <ul>
             ${deliverHTML}
         </ul>
     `;
+
+    document.getElementById('contractOutput').innerHTML = contractHTML;
 }
 
 // event listeners
